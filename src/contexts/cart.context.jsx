9d4bd09b04d85f96d5d478e8a694fa452f4b81c8
AnimasianX <jlin2744@gmail.com>
2022-10-29 21:14:30 -0400

    added cart dropdown menu to show user items in cart and quantity

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,16 +1,37 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, } from "react";
+
+
+const addCartItem = (cartItems, product) => {
+    //find if cart items contains product to add
+    const itemExist = cartItems.find(({ id }) => id === product.id);
+    //if found, increase quantity
+
+    if (itemExist) {
+        return cartItems.map((cartItem) => cartItem.id === product.id ?
+            { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem);
+    }
+    //finally return new array with modified cart items/quantity
+    return [...cartItems, { ...product, quantity: 1 }];
+}
 
 export const CartContext = createContext({
-    cart: null,
-    setCart: () => null,
+    cartItems: null,
+    addItemToCart: () => { },
     isToggled: null,
     setIsToggled: () => { },
 });
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
     const [isToggled, setIsToggled] = useState(false);
-    const value = { cart, setCart, isToggled, setIsToggled };
+
+    const addItemToCart = (product) => {
+
+        setCartItems(addCartItem(cartItems, product))
+    }
+
+    const value = { cartItems, addItemToCart, isToggled, setIsToggled };
 
 
 
